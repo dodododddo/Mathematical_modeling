4987e2e655f129b07738e51345afcfab42b2593a
jr <14523159562@qq.com>
2023-07-28 23:02:04 +0800

    增加了一个没什么用的优化方法

--- 2020A/model.py
+++ 2020A/model.py
@@ -1,6 +1,6 @@
 from utils import * 
 from scipy import linalg
-from math import floor, ceil
+from math import floor, ceil, cos
 
 pos_anchors = [0, 25, 197.5, 339.5, 410.5, 435.5]
 
@@ -18,7 +18,7 @@ class Model():
         self.Nt = floor(self.T / dt) + 1
         self.Nx = floor(l / dx) + 1
         self.alphas = alphas
-        self.rs = [(alpha ** 2) * dt / (dx ** 2) for alpha in alphas]
+        # self.rs = [(alpha ** 2) * dt / (dx ** 2) for alpha in alphas]
         self.dx = dx
         self.dt = dt
         self.l = l
@@ -26,6 +26,7 @@ class Model():
 
     def solve(self):
         '''计算所有时间的T值'''
+        self.rs = [(alpha ** 2) * self.dt / (self.dx ** 2) for alpha in alphas]
         u = np.zeros((self.Nx, self.Nt))
         u[:, 0] = 25
         res = np.ones(self.Nt) * 25
@@ -44,8 +45,28 @@ class Model():
                 res[j + 1] = u[test_pos, j + 1]
 
         return res
-            
     
+    def optimize_each_epoch(self, lr):
+        positions = np.arange(0, 435.5, model.v * model.dt)
+        am_temps = self.pos2am_temp(positions)
+        true_data = get_true_temp()[:, 1]
+        pred = self.solve()
+        for i in range(len(self.step_anchors) - 1):
+            for j in range(self.step_anchors[i] + 38, self.step_anchors[i + 1]):
+                if (pred[j] - true_data[j - 38]).sum() >= 0:
+                    if(true_data[j - 38] < am_temps[j]):
+                        self.alphas[i] -= lr
+                    else:
+                        self.alphas[i] += lr
+                if (pred[j] - true_data[j - 38]).sum() < 0:
+                    if(true_data[j - 38] > am_temps[j]):
+                        self.alphas[i] -= lr
+                    else:
+                        self.alphas[i] += lr
+
+        
+    
+            
 class Optimizer():
     def __init__():
         pass
@@ -60,14 +81,31 @@ class Optimizer():
 class LeastsqOptimizer(Optimizer):
     def __init__():
         super().__init__()
+
+def loss(y, y_hat):
+    return ((y - y_hat) ** 2).sum()
+
+
+
+
    
 if __name__ == '__main__':
 
-    alphas=[6.80e-04, 6.58e-04, 7.74e-04, 5.09e-04, 4.76e-04]
+    alphas=[0.0006613060175224845, 0.0006806842472728309, 0.0007865454578240704, 0.0005173828190274838, 0.0004598235444981665] 
     temps = [175, 195, 235, 255, 25]
     v = 70 / 60
 
     model = Model(v, temps, alphas)
+    lr = 5e-7
+    num_epoches = 100
+    for i in range(num_epoches):
+        lr *= 0.8
+        model.optimize_each_epoch(lr)
+        if i % 10 == 0:
+            print(model.alphas, lr)
+        
+
+
     predicts = model.solve()
 
     trans = pos2temp(temps)
@@ -77,7 +115,7 @@ if __name__ == '__main__':
     true_data = get_true_temp()
     true_data[:,0] = true_data[:, 0] * model.v + 25
     
-    
+    print(loss(true_data[:, 1], predicts[38:]))
     plt.xlabel('time/s')
     plt.ylabel('T/℃')
     plt.plot(positions, am_temps, label=u'ambient_temperature')
